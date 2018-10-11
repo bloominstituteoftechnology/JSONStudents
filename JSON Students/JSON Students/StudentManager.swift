6736268703e41c9299c1fd4c2a9acc962b63fc48
Jerrick Warren <Jerrick.Warren@gmail.com>
2018-10-11 14:59:53 -0700

    app functional working on UI

--- JSON Students/JSON Students/StudentManager.swift
+++ JSON Students/JSON Students/StudentManager.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class StudentManager: NSObject {
     
-    var students: [String] = []
+    var students: [Student] = []
     
     let url = URL(fileURLWithPath: NSHomeDirectory())
         .appendingPathComponent("Documents")
@@ -22,6 +22,14 @@ class StudentManager: NSObject {
         // JSON write to file?
         // do - catch
         
+        do {
+            let studentsEncoded = try JSONEncoder().encode(students)
+            if let string = String(data: studentsEncoded, encoding: .utf8) {
+                print(string)
+            }
+        }catch {
+            print("Error: \(error)")
+        }
     }
     
     func readFromFile() {
@@ -29,6 +37,19 @@ class StudentManager: NSObject {
         // JSON DeCoder
         // JSON read from file?
         // do - catch
+        
+    do {
+            let data = try Data(contentsOf: url)
+            let studentsDecoded = try JSONDecoder().decode([Student].self, from: data)
+            students = studentsDecoded
+        } catch {
+            print("Error: \(error)")
+        }
+    }
+    
+    func create(name: String, age: Int?, cohort: String?) {
+        let student = Student.init(name: name , age: age, cohort: cohort)
+        students.append(student)
         
     }
 }
--- JSON Students/JSON Students/ViewController.swift
+++ JSON Students/JSON Students/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let studentManager = StudentManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        studentManager.readFromFile()
+    }
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var cohortTextField: UITextField!
+    
+    @IBAction func saveButton(_ sender: Any) {
+        guard let name = nameTextField.text,
+            let age = ageTextField.text,
+            let cohort = cohortTextField.text else {return}
+        studentManager.create(name: name, age: Int(age), cohort: cohort)
+        studentManager.writeToFile()
+        
+    }
+    
+    @IBAction func printButton(_ sender: Any) {
+        print(studentManager.students)
+    }
+    
 }
 

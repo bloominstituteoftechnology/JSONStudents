ef1ae418b1c4dd106666138d51ed133338084cb8
Sean Doyle <sdoylelambda@gmail.com>
2018-10-11 15:48:23 -0400

    Code done for student.swift and StudentManager.swift , working on story board

--- JSON Encoding App/JSON Encoding App/StudentManager.swift
+++ JSON Encoding App/JSON Encoding App/StudentManager.swift
@@ -4,17 +4,32 @@ import Foundation
 class StudentManager: NSObject {
     var students: [Student] = []
     
-    var dataURL: URL? {
-        let fm =
-    }
+//    var dataURL: URL? {
+//        let fm =
+//    }
     
     let url = URL(fileURLWithPath: NSHomeDirectory())
         .appendingPathComponent("Documents")
         .appendingPathComponent("students.json")
     
     func writeTOFile() {
-        data.write(to)
+        //data.write(to: url)
+        do {
+            let studentsEncoded = try JSONEncoder().encode(students)
+            try studentsEncoded.write(to: url)
+        } catch {
+            print("Error: \(error)")
+        }
     }
 
     func fromReadFile() {
+      //  Data(contentsOf: url)
+        do {
+            let data = try Data(contentsOf: url)
+            let studentsDecoded = try JSONDecoder().decode([Student].self, from: data)
+            students = studentsDecoded
+        } catch {
+            print("Error: \(error)")
+        }
+}
 }
